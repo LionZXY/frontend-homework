65ed42a47aa15857bcbc70c16c4f7b0da5bb33c1
lionzxy <nikita@kulikof.ru>
2017-08-28 12:30:12 +0300

    Cell char to constant variable

--- source/chess.js
+++ source/chess.js
@@ -1,14 +1,17 @@
+const CELL = '*';
+const INVERTED_CELL = ' ';
+
 function chess(N) {
     if (N == 1) {
         return null
     }
 
-    let outputArr = [' '];
+    let outputArr = [INVERTED_CELL];
 
     for (let i = 1; i < N * 1 + N * N; i++) {
         outputArr.push((i + 1) % N === 0 && i !== 0 ? '\n' : // Условие 1. Если мы достигли края строки, ставим звездочку
-            outputArr[i - 1] === '\n' ? (Math.floor((i + 1) / (N + 1)) % 2 === 0 ? '*' : ' ') : // Условие 2. Если мы вначале строки, ставим знак в зависимости от четности строки
-                outputArr[i - 1] === '*' ? ' ' : '*'); // Условие 3. Текущий знак - инвертированный предыдущий
+            outputArr[i - 1] === '\n' ? (Math.floor((i + 1) / (N + 1)) % 2 === 0 ? CELL : INVERTED_CELL) : // Условие 2. Если мы вначале строки, ставим знак в зависимости от четности строки
+                outputArr[i - 1] === CELL ? INVERTED_CELL : CELL); // Условие 3. Текущий знак - инвертированный предыдущий
     }
 
     return outputArr.join('')
